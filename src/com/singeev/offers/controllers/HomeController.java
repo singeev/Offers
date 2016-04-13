70dbaa7a03991e10942ffda637fd14edf226f8c9
singeev <a.singeev@gmail.com>
2016-04-13 18:16:46 +0300

    Add spring.profiles

--- src/com/singeev/offers/controllers/HomeController.java
+++ src/com/singeev/offers/controllers/HomeController.java
@@ -1,18 +1,22 @@
 package com.singeev.offers.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private static Logger logger = Logger.getLogger(HomeController.class);
 
 	@RequestMapping("/")
 	public String showHome() {
+		logger.info("Show home page...");
 		return "home";
 	}
 
 	@RequestMapping("/error")
 	public String showError() {
+		logger.info("Show error page...");
 		return "error";
 	}
 
